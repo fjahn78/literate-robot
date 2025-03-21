c4d1df921326fb5a8fc01c5bc71cdc27a03a28e7
Frank Jahn <10794823+fjahn78@users.noreply.github.com>
2025-03-21 06:53:15 +0100

    Add files via upload

--- NetzlaufwerkTools/Netzlaufwerktools.psm1
+++ NetzlaufwerkTools/Netzlaufwerktools.psm1
@@ -1,4 +1,4 @@
-$script:logFile = "netzlaufwerke_log.txt"
+$script:logFile = ".\netzlaufwerke.log"
 $script:maxLogSize = 1MB
 
 Get-ChildItem "$PSScriptRoot\public\*.ps1" | ForEach-Object { . $_.FullName }
